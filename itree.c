240e1122768a92905da5aa14ce7ba5ed748f4a4d
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-06-01 18:10:47 -0300

    intersecar

--- itree.c
+++ itree.c
@@ -333,7 +333,7 @@ ITree itree_intersecar(ITree arbol, double izq, double der) {
         // puts("0.1");
         interseccion = itree_intersecar(arbol->left, izq, der);
       }
-    } else if (izq > arbol->der) {
+    } else if (izq > arbol->izq->max) {
       // puts("1");
       if (arbol->right && izq <= arbol->right->max) {
         // puts("1.1");
