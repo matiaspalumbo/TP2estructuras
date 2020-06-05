36dc551476c35310b1ff73605cca9e61cd8d2949
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-06-05 12:26:07 -0300

    ..

--- itree.c
+++ itree.c
@@ -11,29 +11,29 @@ ITree itree_crear() {
   return NULL;
 }
 
-
 void itree_destruir(ITree nodo) {
-  if (nodo) {
+  if (!itree_empty(nodo)) {
     itree_destruir(nodo->left);
     itree_destruir(nodo->right);
     free(nodo);
   }
 }
 
-
 int itree_empty(ITree nodo) {
   return nodo == NULL;
 }
 
+// Devuelve el mayor de los dos doubles.
 double maximo(double n1, double n2) {
   return n1 < n2 ? n2 : n1;
 }
 
+// Dado un nodo calcula el maximo derecho del arbol.
 double calcular_max(ITNodo *nodo) {
-  if (nodo->left && nodo->right)
+  if (!itree_empty(nodo->left) && !itree_empty(nodo->right))
     return maximo(maximo(nodo->der, nodo->left->max), nodo->right->max);
-  else if (nodo->left || nodo->right) {
-    ITNodo *nodoNoNULL = (nodo->left) ? nodo->left : nodo->right;
+  else if (!itree_empty(nodo->left) || !itree_empty(nodo->right)) {
+    ITNodo *nodoNoNULL = (!itree_empty(nodo->left)) ? nodo->left : nodo->right;
     return maximo(nodo->der, nodoNoNULL->max);
   } else
     return nodo->der;
@@ -71,7 +71,6 @@ ITree rotacion_a_izquierda(ITree nodo) {
   return nuevaRaiz;
 }
 
-
 ITree itree_insertar(ITree nodo, double izq, double der) {
   if (itree_empty(nodo)) {
     nodo = malloc(sizeof(ITNodo));
@@ -84,7 +83,7 @@ ITree itree_insertar(ITree nodo, double izq, double der) {
     nodo->left = itree_insertar(nodo->left, izq, der);
   } else if (nodo->izq < izq || (nodo->izq == izq && nodo->der < der)) {
     nodo->right = itree_insertar(nodo->right, izq, der);
-  } else {
+  } else { // Caso en que el intervalo es igual al del nodo, ie ya esta en el arbol y no se debe insertar.
     return nodo;
   }
 
@@ -111,23 +110,54 @@ ITree itree_insertar(ITree nodo, double izq, double der) {
 }
 
 
+
+// BSTree bstree_eliminar(BSTree arbol, int dato) {
+//   if (!btree_empty(arbol)) {
+//     if (arbol->dato == dato) {
+//       BSTree nodoAEliminar = arbol;
+//       if ((arbol->left == NULL) && (arbol->right == NULL)) arbol = NULL;
+//       else if ((arbol->left == NULL) && (arbol->right != NULL)) arbol = arbol->right;
+//       else if ((arbol->left != NULL) && (arbol->right == NULL)) arbol = arbol->left;
+//       else {
+//         BSTree nodo = arbol->right;
+//         BSTree padreNodo = NULL; // apunta al padre de 'nodo'
+//         while (nodo->left != NULL) {
+//           padreNodo = nodo;
+//           nodo = nodo->left; 
+//         } // ahora 'nodo' apunta al minimo nodo del subarbol derecho
+//         if (!btree_empty(padreNodo)) {
+//           padreNodo->left = nodo->right;
+//           nodo->right = arbol->right;
+//         }
+//         nodo->left = arbol->left;
+//         arbol = nodo;
+//       }
+//       free(nodoAEliminar);
+//     } else if (arbol->dato < dato) 
+//         arbol->right = bstree_eliminar(arbol->right, dato);
+//       else 
+//         arbol->left = bstree_eliminar(arbol->left, dato);
+//   }
+//   return arbol;
+// }
+
 ITree itree_eliminar(ITree nodo, double izq, double der) {
   if (itree_empty(nodo))
     return nodo;
   else if (nodo->izq == izq && nodo->der == der) {
-    if (!nodo->left && !nodo->right) {
+    if (itree_empty(nodo->left) && itree_empty(nodo->right)) {
       free(nodo);
       nodo = NULL;
-    } else if (nodo->left && nodo->right) {
+    } else if (!itree_empty(nodo->left) && !itree_empty(nodo->right)) {
       ITree minNodoDerecho = nodo->right;
-      while (minNodoDerecho->left) {
+      while (!itree_empty(minNodoDerecho->left)) {
         minNodoDerecho = minNodoDerecho->left;
       }
       nodo->izq = minNodoDerecho->izq;
       nodo->der = minNodoDerecho->der;
-      nodo->right = itree_eliminar(nodo->right, minNodoDerecho->izq, minNodoDerecho->der);
-    } else if (nodo->left || nodo->right) {
-      ITree hijoNoNULL = (nodo->left) ? nodo->left : nodo->right;
+      nodo->right = itree_eliminar(nodo->right, minNodoDerecho->izq, minNodoDerecho->der); // change
+    } else if (!itree_empty(nodo->left) || !itree_empty(nodo->right)) {
+      ITree hijoNoNULL = (!itree_empty(nodo->left)) ? nodo->left : nodo->right;
       free(nodo);
       nodo = hijoNoNULL;
     }
@@ -137,12 +167,11 @@ ITree itree_eliminar(ITree nodo, double izq, double der) {
     nodo->right = itree_eliminar(nodo->right, izq, der);
   }
   
-  if (!nodo)
-    return nodo;
+  if (itree_empty(nodo)) return nodo;
+
   nodo->altura = 1 + (int) maximo((double) itree_altura(nodo->left),(double) itree_altura(nodo->right));
-
+  
   int balance = itree_balance_factor(nodo);
-
   if (balance < -1 && itree_balance_factor(nodo->right) < 0) {
     // printf("Izq Izq: %0.2f\n", nodo->izq);
     nodo = rotacion_a_izquierda(nodo);
@@ -158,10 +187,9 @@ ITree itree_eliminar(ITree nodo, double izq, double der) {
     nodo->left = rotacion_a_izquierda(nodo->left);
     nodo = rotacion_a_derecha(nodo);
   }
+
   return nodo;
-
 }
-
 
 // int itree_contiene(ITree nodo, int dato) {
 //   if (itree_empty(nodo)) return 0;
@@ -187,9 +215,8 @@ ITree itree_eliminar(ITree nodo, double izq, double der) {
 //  return cantidad;
 // }
 
-
 void imprimir_intervalo(ITree nodo) {
-  if (!itree_empty(nodo)) printf("[%.2lf, %.2lf] ", nodo->izq, nodo->der);
+  if (!itree_empty(nodo)) printf("[%g, %g] ", nodo->izq, nodo->der);
 }
 
 void itree_recorrer_dfs(ITree arbol, FuncionVisitante visit) {
@@ -216,8 +243,6 @@ void itree_recorrer_bfs(ITree arbol, FuncionVisitante visit) {
   }
 }
 
-
-
 void pretty_print_aux(ITree nodo, int space) { 
   if (itree_empty(nodo)) 
     return; 
@@ -235,7 +260,6 @@ void pretty_print(ITree nodo) {
   pretty_print_aux(nodo, 0);
   puts("");puts("");puts("");puts("");
 }
-  
 
 int itree_minimo(ITree arbol) {
   while (arbol->left) {
@@ -243,8 +267,6 @@ int itree_minimo(ITree arbol) {
   }
   return arbol->izq;
 }
-
-
 
 // int itree_acceder(ITree arbol, int pos) {
 //   Pila pila = pila_crear();
@@ -264,15 +286,14 @@ int itree_minimo(ITree arbol) {
 //   return dato;
 // }
 
-
 ITree itree_intersecar(ITree arbol, double izq, double der) {
   ITree interseccion = NULL;
-  if (arbol) {
+  if (!itree_empty(arbol)) {
     if (der < arbol->izq) {
-      if (arbol->left && izq <= arbol->left->max)
+      if (!itree_empty(arbol->left) && izq <= arbol->left->max)
         interseccion = itree_intersecar(arbol->left, izq, der);
     } else if (izq > arbol->der) {
-      if (arbol->left && izq <= arbol->left->max)
+      if (!itree_empty(arbol->left) && izq <= arbol->left->max)
         interseccion = itree_intersecar(arbol->left, izq, der);
       else 
         interseccion = itree_intersecar(arbol->right, izq, der);
